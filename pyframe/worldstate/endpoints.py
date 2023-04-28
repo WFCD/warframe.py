72f8aa1a9c1b19da9f6bc52e55873594be1d3244
Kai Konze <kaicrafter2000@gmail.com>
2023-04-28 15:13:46 +0200

    Added proper typehinting + made them constants

--- pyframe/worldstate/endpoints.py
+++ pyframe/worldstate/endpoints.py
@@ -1,37 +1,39 @@
-GENERAL = "https://api.warframestat.us/pc"
+from typing import Final
+
+GENERAL: Final[str] = "https://api.warframestat.us/pc"
 
 # Open Worlds
-CETUS = "https://api.warframestat.us/pc/cetusCycle"
-CAMBION_DRIFT = "https://api.warframestat.us/pc/cambionCycle"
-ORB_VALLIS = "https://api.warframestat.us/pc/vallisCycle"
+CETUS: Final[str] = "https://api.warframestat.us/pc/cetusCycle"
+CAMBION_DRIFT: Final[str] = "https://api.warframestat.us/pc/cambionCycle"
+ORB_VALLIS: Final[str] = "https://api.warframestat.us/pc/vallisCycle"
 
 # Darvo
-DAILY_DEAL = "https://api.warframestat.us/pc/dailyDeals"
-FLASH_SALES = "https://api.warframestat.us/pc/flashSales"
+DAILY_DEAL: Final[str] = "https://api.warframestat.us/pc/dailyDeals"
+FLASH_SALES: Final[str] = "https://api.warframestat.us/pc/flashSales"
 
 # Alert related
-ALERTS = "https://api.warframestat.us/pc/alerts"
-ARBITRATION = "https://api.warframestat.us/pc/arbitration"
-INVASIONS = "https://api.warframestat.us/pc/invasions"
-VOID_TRADER = "https://api.warframestat.us/pc/voidTrader"
-EVENTS = "https://api.warframestat.us/pc/events"
-NEWS = "https://api.warframestat.us/pc/news"
+ALERTS: Final[str] = "https://api.warframestat.us/pc/alerts"
+ARBITRATION: Final[str] = "https://api.warframestat.us/pc/arbitration"
+INVASIONS: Final[str] = "https://api.warframestat.us/pc/invasions"
+VOID_TRADER: Final[str] = "https://api.warframestat.us/pc/voidTrader"
+EVENTS: Final[str] = "https://api.warframestat.us/pc/events"
+NEWS: Final[str] = "https://api.warframestat.us/pc/news"
 
 # Daily stuff
-ARCHON_HUNT = "https://api.warframestat.us/pc/archonHunt"
-STEEL_PATH = "https://api.warframestat.us/pc/steelPath"
-SORTIE = "https://api.warframestat.us/pc/sortie"
+ARCHON_HUNT: Final[str] = "https://api.warframestat.us/pc/archonHunt"
+STEEL_PATH: Final[str] = "https://api.warframestat.us/pc/steelPath"
+SORTIE: Final[str] = "https://api.warframestat.us/pc/sortie"
 
 # Misc
-CONCLAVE = "https://api.warframestat.us/pc/conclaveChallenges"
-CONSTRUCTION_PROGRESS = "https://api.warframestat.us/pc/constructionProgress"
-EARTH_CYCLE = "https://api.warframestat.us/pc/earthCycle"
-FISSURES = "https://api.warframestat.us/pc/fissures"
-GLOBAL_UPGRADES = "https://api.warframestat.us/pc/globalUpgrades" # don't really know what this is for
-KUVA_MISSIONS = "https://api.warframestat.us/pc/kuva"
-NIGHTWAVE = "https://api.warframestat.us/pc/nightwave"
-PERSISTENT_ENEMIES = "https://api.warframestat.us/pc/persistentEnemies"
-RIVENS = "https://api.warframestat.us/pc/rivens"
-SENTIENT_OUTPOST = "https://api.warframestat.us/pc/sentientOutposts"
-SANCTUARY = "https://api.warframestat.us/pc/simaris"
-SYNDICATE_MISSIONS = "https://api.warframestat.us/pc/syndicateMissions"
+CONCLAVE: Final[str] = "https://api.warframestat.us/pc/conclaveChallenges"
+CONSTRUCTION_PROGRESS: Final[str] = "https://api.warframestat.us/pc/constructionProgress"
+EARTH_CYCLE: Final[str] = "https://api.warframestat.us/pc/earthCycle"
+FISSURES: Final[str] = "https://api.warframestat.us/pc/fissures"
+GLOBAL_UPGRADES: Final[str] = "https://api.warframestat.us/pc/globalUpgrades" # don't really know what this is for
+KUVA_MISSIONS: Final[str] = "https://api.warframestat.us/pc/kuva"
+NIGHTWAVE: Final[str] = "https://api.warframestat.us/pc/nightwave"
+PERSISTENT_ENEMIES: Final[str] = "https://api.warframestat.us/pc/persistentEnemies"
+RIVENS: Final[str] = "https://api.warframestat.us/pc/rivens"
+SENTIENT_OUTPOST: Final[str] = "https://api.warframestat.us/pc/sentientOutposts"
+SANCTUARY: Final[str] = "https://api.warframestat.us/pc/simaris"
+SYNDICATE_MISSIONS: Final[str] = "https://api.warframestat.us/pc/syndicateMissions"
