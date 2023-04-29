e4e066e1706937eff4faf2ff13215845ac167fbc
Kai Konze <kaicrafter2000@gmail.com>
2023-04-29 14:16:46 +0200

    added Node, Faction and MissionType

--- pyframe/worldstate/_types/base_objects.py
+++ pyframe/worldstate/_types/base_objects.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import Enum
+
 from typing_extensions import Self
 
 
@@ -12,3 +15,53 @@ class WorldstateObject(ABC):
     @abstractmethod
     def _from_response(cls, response: Record) -> Self:
         pass
+
+@dataclass
+class Node:
+    node: str
+    node_key: str
+
+class Faction(Enum):
+    Grineer = 1
+    Corpus = 2
+    Infested = 3
+    Orokin = 4
+    Sentient = 5
+    Narmer = 6
+
+class MissionType(Enum):
+    Arena = 1
+    Assassination = 2
+    Assault = 3
+    Capture = 4
+    Defection = 5
+    Defense = 6
+    MirrorDefense = 7
+    Disruption = 8
+    Excavation = 9
+    Exterminate = 10
+    FreeRoam = 11
+    Heist = 12
+    IsolationVault = 13
+    Hijack = 14
+    InfestedSalvage = 15
+    Interception = 16
+    Junction = 17
+    MobileDefense = 18
+    Orphix = 19
+    Pursuit = 20
+    Rescue = 21
+    Rush = 22
+    Sabotage = 23
+    HiveSabotage = 24
+    OrokinSabotage = 25
+    ReactorSabotage = 26
+    SealabSabotage = 27
+    SanctuaryOnslaught = 28
+    Skirmish = 29
+    Spy = 30
+    Survival = 31
+    VoidArmageddon = 32
+    VoidCascade = 33
+    VoidFlood = 34
+    Volatile = 35
