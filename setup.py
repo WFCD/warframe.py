556e7db7274bb2bdd39e5c89bed41666775a8149
Kai Konze <kaicrafter2000@gmail.com>
2023-06-05 22:38:42 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+with open("requirements.txt") as reqs_file:
+    requirements = reqs_file.readlines()
+
+setup(
+    name="pyframe",
+    version="0.1.0",
+    author="Mettwasser",
+    description="An asynchronous Python API wrapper for the Warframestat API and (later) the warframe.market API.",
+    packages=["pyframe.worldstate"],
+    package_data={"pyframe.worldstate": ["common/*", "enums/*", "models/*"]},
+    install_requires=requirements,
+    license="MIT",
+    python_requires=">=3.9.0",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
