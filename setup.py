32a7b2cf494d608355bce7bdeae17364d3defd35
Kai Konze <kaicrafter2000@gmail.com>
2023-06-06 22:11:59 +0200

    refactor: added more keywords, changed packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import find_namespace_packages, setup
 
 
 with open("requirements.txt") as reqs_file:
@@ -8,15 +8,17 @@ setup(
     name="pyframe",
     version="0.1.0",
     author="Mettwasser",
+    url="https://github.com/Mettwasser/pyframe",
     description="An asynchronous Python API wrapper for the Warframestat API and (later) the warframe.market API.",
-    packages=["pyframe.worldstate"],
-    package_data={"pyframe.worldstate": ["common/*", "enums/*", "models/*"]},
+    packages=find_namespace_packages(include=["pyframe*"]),
     install_requires=requirements,
     license="MIT",
     python_requires=">=3.9.0",
     classifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
+    keywords="Warframe API worldstate market wrapper",
 )
