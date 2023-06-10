8960b7f3ceed272c36e29db7cafb0e19cf2dcf94
Kai Konze <kaicrafter2000@gmail.com>
2023-06-10 21:39:17 +0200

    fix: Alerts now work.

--- warframe/worldstate/models/alert.py
+++ warframe/worldstate/models/alert.py
@@ -11,21 +11,27 @@ class Alert(MultiQueryModel):
     __endpoint__ = "/alerts"
 
     # required
+    activation: datetime
+    "The time the mission began"
+
+    expiry: datetime
+    "The time the mission ends"
+
     mission: Mission
     "The mission that corresponds to this Alert"
-    expired: bool
-    "Whether the mission is expired or not"
+
     reward_types: List[ItemRewardType]
     "A list of reward types"
 
     # optional
-    activation: Optional[datetime] = None
-    "The time the mission began"
-    expiry: Optional[datetime] = None
-    "The time the mission ends"
     start_string: Optional[str] = None
     "Short-time-formatted duration string representing the start of the alert"
+
     active: Optional[bool] = None
     "Whether the alert is still active or not"
+
     eta: Optional[str] = None
     "Short-formatted string estimating the time until the Alert is closed"
+
+    expired: Optional[bool] = None
+    "Whether the mission is expired or not"
--- warframe/worldstate/models/mission.py
+++ warframe/worldstate/models/mission.py
@@ -10,37 +10,54 @@ __all__ = ["Mission"]
 class Mission(SingleQueryModel):
     # required
     reward: Reward
+
     "The mission's reward"
+
     node: str
     "The localized node string"
+
     faction: Faction
     "The faction that houses the node/mission"
-    max_enemy_level: int
-    min_enemy_level: int
-    max_wave_num: int
+
     type: MissionType
     "The MissionType of the given mission (Capture, Spy, etc.)"
+
     nightmare: bool
     "Whether the mission is a nightmare mission"
+
     archwing_required: bool
     "Whether an archwing is required in order to play the mission"
+
     description: str
     "The mission's description"
 
     # optional
+    max_enemy_level: Optional[int] = None
+
+    min_enemy_level: Optional[int] = None
+
+    max_wave_num: Optional[int] = None
+
     is_sharkwing: Optional[bool] = None
     "Whether the mission takes place in a submerssible mission"
+
     enemy_spec: Optional[str] = None
     "Enemy specification for the mission"
+
     level_override: Optional[str] = None
     "Override for the map on this mission"
+
     advanced_spawners: Optional[List[str]] = None
     "Array of strings denoting extra spawners for a mission"
+
     required_items: Optional[List[str]] = None
     "Items required to enter the mission"
+
     consume_required_items: Optional[bool] = None
     "Whether the required items are consumed"
+
     leaders_always_allowed: Optional[bool] = None
     "Whether leaders are always allowed"
+
     level_auras: Optional[List[str]] = None
     "Affectors for this mission"
