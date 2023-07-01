fe9d45dac8497d99165939f57f3fa925093b1ce3
Kai Konze <kaicrafter2000@gmail.com>
2023-07-01 10:52:21 +0200

    docs: added an example for a custom logger

---
+++ examples/worldstate/custom_logger.py
@@ -0,0 +1,25 @@
+# queries are just another way to get the data to the corresponding objects.
+# this is really just personal preference.
+
+import asyncio
+import logging
+
+from warframe.worldstate import WorldstateClient, WorldstateLogger
+from warframe.worldstate.models import Cetus
+
+
+async def main():
+    # Note that the default logger is pretty much empty (nothing will be logged)
+    # so if you want to make use of the logger, make your own:
+    logger = WorldstateLogger("name whatever you want", logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+
+    async with WorldstateClient(logger=logger) as client:  # pass the logger
+        cetus = await client.query(Cetus)
+
+        print(cetus)
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
